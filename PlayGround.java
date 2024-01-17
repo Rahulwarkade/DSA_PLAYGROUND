2cca41407489f1d2338f629924f573170c9e3883
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-18 05:14:16 +0530

    Post Order Traversal

--- PlayGround.java
+++ PlayGround.java
@@ -49,11 +49,15 @@ public class PlayGround
         preOrder(root.left);
         preOrder(root.right);
     }
-    public static void fun(Node root)
+
+    public static void postOrder(Node root)
     {
-        root = root.left;
-        System.out.println(root);
+        if(root==null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
     }
+
     public static void main(String args[])
     {
         int[] arr = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -61,6 +65,7 @@ public class PlayGround
         Node node = buildTree(arr);
 
         preOrder(node);
+        System.out.println();
         postOrder(node);
     }
 }
