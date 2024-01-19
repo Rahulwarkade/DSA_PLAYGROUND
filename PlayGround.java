8e47a4df9f1758b85d320bdce47b0b4d7b213300
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-19 13:47:11 +0530

    Height of Binary Tree in O(N)

--- PlayGround.java
+++ PlayGround.java
@@ -2,47 +2,64 @@ import java.util.*;
 
 public class PlayGround
 {
-    public static int chocola(Integer[] hor,Integer[] ver)
+    public static class Node
     {
-        int n = hor.length;
-        int m = ver.length;
-        Arrays.sort(hor,Comparator.reverseOrder());
-        Arrays.sort(ver,Comparator.reverseOrder());
-        int hc =0,vc=0;
-        int hp =1,vp =1;
-        int cost = 0;
-        while(hc<n && vc<m)
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data)
         {
-            if(hor[hc]>ver[vc])
-            {
-                cost += (hor[hc]*hp);
-                hc++;
-                vp++;
-            }else{
-                cost += (ver[vc]*vp);
-                vc++;
-                hp++;
-            }
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
-        while(hc<n)
-        {
-            cost += (hor[hc]*hp);
-            hc++;
-            vp++;
-        }
-        while(vc<m)
-        {
-            cost += (ver[vc]*vp);
-            vc++;
-            hp++;
-        }
-        return cost;
+    }
+    public static Node root;
+    public static int idx = -1;
+    public static Node buildTree(int[] arr)
+    {
+        idx++;
+        if(arr[idx]==-1) return null;
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return newNode;
+    }
+
+    public static void preOder(Node node)
+    {
+        if(node==null) return;
+
+        System.out.print(node.data+" ");
+        preOder(node.left);
+        preOder(node.right);
+    }
+    public static void inOrder(Node node)
+    {
+        if(node==null) return;
+
+        inOrder(node.left);
+        System.out.print(node.data+" ");
+        inOrder(node.right);
+    }
+
+    public static int heightOfTree(Node node)
+    {
+        if(node==null) return 0;
+        int lefth = heightOfTree(node.left);
+        int righth = heightOfTree(node.right);
+        return (Math.max(lefth,righth) + 1);
     }
     public static void main(String args[])
     {
-       Integer[] horizontalcuts = {4,3,2,1,1};
-       Integer[] verticalcuts = {4,2,1};
+        int[] arr = {1,2,4,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1};
 
-       System.out.println(chocola(horizontalcuts,verticalcuts));
+        root = buildTree(arr);
+        preOder(root);
+        System.out.println();
+        System.out.println("Height of Tree is = " + heightOfTree(root));
     }
 }
