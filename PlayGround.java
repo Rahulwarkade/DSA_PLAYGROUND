b0e680170f9836a2e4098ad4824fc6c27639c7b1
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-19 13:55:51 +0530

    Sum of Nodes In A Tree in O(N)

--- PlayGround.java
+++ PlayGround.java
@@ -46,10 +46,10 @@ public class PlayGround
         inOrder(node.right);
     }
 
-    public static int countOfNodes(Node node)
+    public static int sumOfNodes(Node node)
     {
         if(node == null) return 0;
-        return countOfNodes(node.left) + countOfNodes(node.right) + 1;
+        return sumOfNodes(node.left) + sumOfNodes(node.right) + node.data;
     }
     public static void main(String args[])
     {
@@ -58,6 +58,6 @@ public class PlayGround
         root = buildTree(arr);
         preOder(root);
         System.out.println();
-        System.out.println("Count of Nodes In A Tree is = " + countOfNodes(root));
+        System.out.println("Sum of Nodes In A Tree is = " + sumOfNodes(root));
     }
 }
