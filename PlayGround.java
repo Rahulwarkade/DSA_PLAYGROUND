58e19ca6833372ced6c3d2c6b4cb7a6b999c4eba
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-20 07:24:30 +0530

    Mininum Distance B/W Nodes in O(N)

--- PlayGround.java
+++ PlayGround.java
@@ -40,23 +40,45 @@ public class PlayGround
 
     public static Node lca(Node node,int n1,int n2)
     {
-        if(node==null || node.data == n1 || node.data== n2) return node;
+        if(node==null || node.data==n1 || node.data==n2) return node;
 
         Node leftlca = lca(node.left,n1,n2);
         Node rightlca = lca(node.right,n1,n2);
 
         if(leftlca==null) return rightlca;
         if(rightlca==null) return leftlca;
-
+        
         return node;
-    }   
+    }
+    public static int dist(Node ancestor,int n)
+    {
+        if(ancestor==null) return -1;
+        if(ancestor.data==n) return 0;
 
+        int leftdist = dist(ancestor.left,n);
+        int rightdist = dist(ancestor.right,n);
+        if(leftdist!=-1)
+        {
+            return leftdist+1;
+        }
+        if(rightdist!=-1){
+            return rightdist+1;
+        }
+        return -1;
+    }
+    public static int minDist(Node node,int n1,int n2)
+    {
+        if(node==null) return -1;
+        Node ancestor = lca(node,n1,n2);
+
+        return dist(ancestor,n1)+dist(ancestor,n2);
+    }
     public static void main(String args[])
     {
         int[] tree = {1,2,3,-1,-1,4,-1,-1,6,5,-1,-1,7,-1,-1};
 
         root = buildTree(tree);
 
-        System.out.println("Lowest Common Ancestor = "+ lca(root,4,3).data);
+        System.out.println("Min Distance b/w Nodes = "+ minDist(root,4,5));
     }
 }
