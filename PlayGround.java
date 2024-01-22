6a523f9df2b306559977742661c5f2a5cb6a820d
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-22 18:23:09 +0530

    Print in Range in O(H)

--- PlayGround.java
+++ PlayGround.java
@@ -43,46 +43,25 @@ public class PlayGround
         inOrder(root.right);
     }
 
-    public static void preOrder(Node root)
+    public static void printInRange(Node root,int k1,int k2)
     {
         if(root==null) return;
-        System.out.print(root.data+" ");
-        preOrder(root.left);
-        preOrder(root.right);
+        if(root.data>=k1 && root.data<=k2)
+        {
+            printInRange(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right,k1,k2);
+        }
+        else if(root.data<k1)
+        {
+            printInRange(root.right,k1,k2);
+        }
+        else if(root.data>k2)
+        {
+            printInRange(root.left,k1,k2);
+        }
     }
-    public static Node findInOrderSuccessor(Node root)
-    {
-        while(root.left!=null)
-        {
-            root = root.left;
-        }
-        return root;
-    }
 
-    public static Node delete(Node root,int key)
-    {
-        if(root.data == key)
-        {
-            if(root.left == null && root.right ==null) return null;
-
-            if(root.left==null) return root.right;
-
-            if(root.right==null) return root.left;
-
-            Node inOrderSuccessor = findInOrderSuccessor(root.right);
-            root.data = inOrderSuccessor.data;
-            root.right = delete(root.right,inOrderSuccessor.data);
-        }
-        else if(root.data<key)
-        {
-            root.right = delete(root.right,key);
-        }
-        else
-        {
-            root.left = delete(root.left,key);
-        }
-        return root;
-    }
     public static void main(String args[])
     {
         int[] tree = {8,5,10,3,6,11,1,4,14};
@@ -94,9 +73,8 @@ public class PlayGround
             root = insert(root,tree[i]);
         }
 
-        root = delete(root,8);
-        preOrder(root);
-        System.out.println();
         inOrder(root);
+        System.out.println();
+        printInRange(root, 6, 12);
     }
 }
