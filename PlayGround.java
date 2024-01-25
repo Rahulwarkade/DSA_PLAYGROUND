5c7b5b98d94528421af190c8a171d9bad944ec45
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-25 16:42:55 +0530

    Connect N ropes with minimum cost

--- PlayGround.java
+++ PlayGround.java
@@ -2,42 +2,24 @@ import java.util.*;
 
 public class PlayGround
 {  
-    public static class Point implements Comparable<Point>
-    {
-        int x;
-        int y;
-        int dist;
-        int idx;
-        public Point(int x,int y,int dist,int idx)
-        {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(Point obj2)
-        {
-            return this.dist - obj2.dist;
-        }
-    }
     public static void main(String args[])
     {
-        int[][] arr = {{3,3},{5,-1},{-2,4}};
-        int k = 2;
+        int[] ropes = {2,3,3,4,6};
+        int cost = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-
-        for(int i=0; i<arr.length; i++)
+        for(int i=0; i<ropes.length; i++)
         {
-            int x= arr[i][0];
-            int y = arr[i][1];
-            int dist = x*x + y*y;
-            pq.add(new Point(x,y,dist,i));
+            pq.add(ropes[i]);
         }
 
-        for(int i=0; i<k; i++)
-            System.out.println("C"+pq.remove().idx);
+        while(pq.size()>1)
+        {
+            int min1 = pq.remove();
+            int min2 = pq.remove();
+            cost += min1+min2;
+            pq.add(min1+min2);
+        }
+        System.out.println("Cost of connecting n ropes = " + cost);
     }
 }
