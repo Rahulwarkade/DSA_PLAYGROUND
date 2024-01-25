d8f5abecc2d11bc6c29f1e1de7c965aa952076b5
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-25 18:10:49 +0530

    Weakest Soldier

--- PlayGround.java
+++ PlayGround.java
@@ -2,24 +2,46 @@ import java.util.*;
 
 public class PlayGround
 {  
+    public static class Soldier implements Comparable<Soldier>
+    {
+        int count;
+        int idx;
+        public Soldier(int count,int idx)
+        {
+            this.count = count;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Soldier o2)
+        {
+            if(this.count==o2.count)
+            {
+                return this.idx - o2.idx;
+            }
+            return this.count - o2.count;
+        }
+    }
     public static void main(String args[])
     {
-        int[] ropes = {2,3,3,4,6};
-        int cost = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[][] soldiers = {{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}};
+        int k = 2;
+        PriorityQueue<Soldier> pq = new PriorityQueue<>();
 
-        for(int i=0; i<ropes.length; i++)
+        for(int i=0; i<soldiers.length; i++)
         {
-            pq.add(ropes[i]);
+            int count =0;
+            for(int j=0; j<soldiers[0].length; j++)
+            {
+                count += (soldiers[i][j]==1)?1 :0;
+            }
+            pq.add(new Soldier(count,i));
         }
 
-        while(pq.size()>1)
+        for(int i=0; i<k; i++)
         {
-            int min1 = pq.remove();
-            int min2 = pq.remove();
-            cost += min1+min2;
-            pq.add(min1+min2);
+            System.out.println("R"+pq.remove().idx);
         }
-        System.out.println("Cost of connecting n ropes = " + cost);
+     
     }
 }
