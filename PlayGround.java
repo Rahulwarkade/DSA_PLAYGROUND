1a0e36d512fb277191d572cf4df9e35b797d1f54
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-26 22:58:10 +0530

    Subarray Sum equal to K

--- PlayGround.java
+++ PlayGround.java
@@ -4,25 +4,25 @@ public class PlayGround
 {  
     public static void main(String args[])
     {
-        int[] arr = {15,-2,2,-8,1,7,10};
+        int[] arr = {10,2,-2,-20,10};
+        int k = -10;
+        HashMap<Integer,Integer> map  = new HashMap<>();
+        map.put(0,1);
+
         int sum = 0;
-        int len = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i=0; i<arr.length; i++)
+        int ans = 0;
+        for(int j=0; j<arr.length; j++)
         {
-            sum += arr[i];
+            sum += arr[j];
 
-            if(map.containsKey(sum))
+            if(map.containsKey(sum-k))
             {
-                len = Math.max(len,i-map.get(sum));
+                ans += map.get(sum-k);
             }
-            else
-            {
-                map.put(sum, i);
-            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
 
-        System.out.println("Largest Subarray with 0 sum = "+ len);
+        System.out.println("Subarray sum equal to K = "+ans);
+        
     }
 }
