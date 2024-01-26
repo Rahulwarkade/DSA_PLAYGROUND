caa2d9d152478e3307eaf2358d29cca89f55a8f8
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-26 15:03:43 +0530

    Union and Intersection of 2 arrays in O(n)

--- PlayGround.java
+++ PlayGround.java
@@ -4,12 +4,29 @@ public class PlayGround
 {  
     public static void main(String args[])
     {
-        int[] num = {4,3,2,5,6,7,3,4,2,1};
+        int[] arr1 = {7,3,9};
+        int[] arr2 = {6,3,9,2,9,4};
         HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int value : arr1) set.add(value);
+        for(int value : arr2) set.add(value);
 
-        for(int value : num)
-            set.add(value);
+        System.out.println("Union is = "+ set.size());
+
+        set = new HashSet<>();
+        for(int value : arr1) set.add(value);
+
+        int count = 0;
+        for(int value : arr2)
+        {
+            if(set.contains(value))
+            {
+                count++;
+                set.remove(value);
+            }
+        }
+
+        System.out.println("Intersection = "+count);
         
-        System.out.println("Distinct Elements = "+ set.size());
     }
 }
