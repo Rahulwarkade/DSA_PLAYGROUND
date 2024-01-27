68a351f0397f731f41e7f5157d2d4438e08e4343
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-27 18:40:41 +0530

    Insert in Tries O(L)

--- PlayGround.java
+++ PlayGround.java
@@ -2,27 +2,45 @@ import java.util.*;
 
 public class PlayGround
 {  
+    public static class Node 
+    {
+        Node[] children = new Node[26];
+        boolean eow  = false;
+
+        public Node()
+        {
+            for(int i=0; i<26; i++)
+                this.children[i] = null;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word)
+    {
+        Node temp = root;
+        for(int i=0; i<word.length(); i++)
+        {
+            char ch = word.charAt(i);
+            int idx = ch - 'a';
+            if(temp.children[idx]==null)
+            {
+                temp.children[idx] = new Node();
+            }
+            temp = temp.children[idx];
+        }
+        temp.eow = true;
+    }
+
+
     public static void main(String args[])
     {
-        int[] arr = {10,2,-2,-20,10};
-        int k = -10;
-        HashMap<Integer,Integer> map  = new HashMap<>();
-        map.put(0,1);
+        String[] words = {"the","a","there","their","any","thee"};
 
-        int sum = 0;
-        int ans = 0;
-        for(int j=0; j<arr.length; j++)
+        for(int i=0; i<words.length; i++)
         {
-            sum += arr[j];
-
-            if(map.containsKey(sum-k))
-            {
-                ans += map.get(sum-k);
-            }
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            insert(words[i]);
         }
 
-        System.out.println("Subarray sum equal to K = "+ans);
-        
     }
 }
