b4d3e0655ebde7f501c416a0c8ab47c94b89766e
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 21:18:32 +0530

    fibonacci using Recursion O(2^N)

--- PlayGround.java
+++ PlayGround.java
@@ -2,41 +2,15 @@ import java.util.*;
 
 public class PlayGround
 {  
- 
-    public static void floodFillUtil(int[][] image,int sr,int sc,int newCol,int orgCol,boolean[][] vis)
+    
+    public static int fib(int n)
     {
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || vis[sr][sc] || image[sr][sc]!=orgCol) return;
-        image[sr][sc] = newCol;
-        vis[sr][sc] = true;
-
-        floodFillUtil(image,sr-1,sc,newCol,orgCol,vis);
-        floodFillUtil(image,sr+1,sc,newCol,orgCol,vis);
-        floodFillUtil(image,sr,sc-1,newCol,orgCol,vis);
-        floodFillUtil(image,sr,sc+1,newCol,orgCol,vis);
-    }
-    public static int[][] floodFill(int[][] image,int sr,int sc,int newCol)
-    {
-        boolean[][] vis = new boolean[image.length][image[0].length];
-        for(int i=0; i<vis.length; i++)
-            Arrays.fill(vis[i],false);
-        
-        floodFillUtil(image,sr,sc,newCol,image[sr][sc],vis);
-
-        return image;
+        if(n==0 || n==1) return n;
+        return fib(n-1)+fib(n-2);
     }
     public static void main(String args[])
     {
-        int[][] image = {{1,1,1},
-                             {1,1,0},
-                             {1,0,1}};
-
-        image = floodFill(image,1,1,2);
-
-        for(int i=0; i<image.length; i++)
-        {
-            for(int j=0; j<image[i].length; j++)
-                System.out.print(image[i][j]+" ");
-            System.out.println();
-        }
+        int n = 5;
+        System.out.println(fib(n));
     }
 }
