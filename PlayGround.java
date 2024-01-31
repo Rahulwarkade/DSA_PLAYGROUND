6996f3a1beb6b734e00de7b6f682a5a1aed92464
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 21:39:38 +0530

    Tabulation in DP buttom up Approach

--- PlayGround.java
+++ PlayGround.java
@@ -5,11 +5,14 @@ public class PlayGround
     
     public static int climbingStairs(int n,int[] dp)
     {
-        if(n==0) return 1;
-        if(n<0) return 0;
+        dp[0] = 1;
+        dp[1] = 1;
 
-        if(dp[n]!=0) return dp[n];
-        return dp[n] = climbingStairs(n-1, dp)+climbingStairs(n-2, dp);
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
     public static void main(String args[])
     {
