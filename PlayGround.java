bc19e2a0ac97429a1e2ded8b4e60e83af0d120e8
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 21:36:36 +0530

    Climbing Stairs using Dynamic Programming

--- PlayGround.java
+++ PlayGround.java
@@ -3,16 +3,18 @@ import java.util.*;
 public class PlayGround
 {  
     
-    public static int climbingStairs(int n)
+    public static int climbingStairs(int n,int[] dp)
     {
         if(n==0) return 1;
         if(n<0) return 0;
 
-        return climbingStairs(n-1)+climbingStairs(n-2);
+        if(dp[n]!=0) return dp[n];
+        return dp[n] = climbingStairs(n-1, dp)+climbingStairs(n-2, dp);
     }
     public static void main(String args[])
     {
         int n = 5;
-        System.out.println(climbingStairs(n));
+        int[] dp = new int[n+1];
+        System.out.println(climbingStairs(n,dp));
     }
 }
