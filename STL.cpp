c9c001d3e6aa2eb0b72e67ee5790cb658dac7d4b
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 21:27:30 +0530

    Inclusion and Exclusion Principle

--- STL.cpp
+++ STL.cpp
@@ -1,81 +1,31 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void merge(int arr[],int l,int  mid,int r)
+
+int divisible(int n, int a,int b)
 {
-  int n1 = mid-l+1;
-  int n2 = r-mid;
-
-  int arr1[n1];
-  int arr2[n2];
-
-  for(int i=0; i<n1; i++)
-      arr1[i] = arr[l+i];
-  for(int i=0; i<n2; i++)
-      arr2[i] = arr[mid+1+i];
-
-  int i = 0;
-  int j = 0;
-  int k = l;
-
-  while(i<n1 && j<n2)
-    {
-      if(arr1[i]>arr2[j])
-      {
-        arr[k] = arr2[j];
-        k++;
-        j++;
-      }
-      else
-      {
-        arr[k] = arr1[i];
-        k++;
-        i++;
-      }
-    }
-  while(i<n1)
-    {
-      arr[k] = arr1[i];
-      k++;
-      i++;
-    }
-  while(j<n2)
-    {
-      arr[k] = arr2[j];
-      k++;
-      j++;
-    }
-    
+int c1 = n/a;
+int c2 = n/b;
+  int c3 = n/(a*b);
+  return c1+c2-c3;
 }
-
-
-void mergeSort(int arr[],int l,int r)
+int gcd(int a,int b)
 {
-  if(l<r)
-  {
-    int mid = (l+r)/2;
-    
-    mergeSort(arr,l,mid);
-    
-    mergeSort(arr,mid+1,r);
-    
-    merge(arr,l,mid,r);
-  }
+  int rem;
+  while(b)
+    {
+      rem = a%b;
+      a = b;
+      b=rem;
+    }
+  return a;
 }
-
-
 int main()
 {
-  int n;
-  cin>>n;
-  int arr[n];
-  for(int i=0; i<n; i++)
-    cin>>arr[i];
-  mergeSort(arr,0,n);
-
-  for(int i=0; i<n; i++)
-    cout<<arr[i]<<" ";
-
-  cout<<endl;
-
-  return 0;
+  // int n,a,b;
+  // cin>>n>>a>>b;
+  // cout<<divisible(n,a,b)<<endl;
+  // Eclid algorith to find greates common divisor
+  int a,b;
+  cin>>a>>b;
+  cout<<gcd(a,b);
 }
