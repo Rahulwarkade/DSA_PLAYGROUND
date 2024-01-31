2bcd9cbf711395448ba27c97c692d5328ed38dee
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 21:28:43 +0530

    Sieve Eratosthenes

--- STL.cpp
+++ STL.cpp
@@ -1,31 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int divisible(int n, int a,int b)
+void primeSieve(int n)
 {
-int c1 = n/a;
-int c2 = n/b;
-  int c3 = n/(a*b);
-  return c1+c2-c3;
-}
-int gcd(int a,int b)
-{
-  int rem;
-  while(b)
+  int arr[n];
+  for(int i=0; i<n; i++)
+    arr[i] = 0;
+  for(int i=2; i<n; i++)
     {
-      rem = a%b;
-      a = b;
-      b=rem;
+      if(arr[i]==0)
+      {
+        for(int j=i*i; j<n; j+=i)
+          {
+            arr[j] = 1;
+          }
+      }
     }
-  return a;
+  for(int i=2; i<n; i++)
+    {
+      if(arr[i]==0)
+        cout<<i<<" ";
+    }
+}
+
+void primeFactor(int n)
+{
+  int spf[n];
+  for(int i=2; i<=n; i++)
+    spf[i] = i;
+
+  for(int i=2; i<=n; i++)
+    {
+      if(spf[i]==i)
+      {
+        for(int j=i*i; j<=n; j+=i)
+          {
+            if(spf[j]==j)
+              spf[j]=i;
+          }
+      }
+    }
+  while(n!=1)
+    {
+      cout<<spf[n]<<" ";
+      n/=spf[n];
+    }
 }
 int main()
 {
-  // int n,a,b;
-  // cin>>n>>a>>b;
-  // cout<<divisible(n,a,b)<<endl;
-  // Eclid algorith to find greates common divisor
-  int a,b;
-  cin>>a>>b;
-  cout<<gcd(a,b);
+  int n;
+  cin>>n;
+  // primeSieve(n);
+  primeFactor(n);
+  return 0;
 }
