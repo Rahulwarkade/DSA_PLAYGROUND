64ce9f57d79ecfbfd3d3a025b628090ff05fd875
Rahul Warkade <rahulwarkade954@gmail.com>
2024-01-31 15:57:36 +0530

    Krushkal's Algorithm O(V+ElogE)

--- PlayGround.java
+++ PlayGround.java
@@ -2,7 +2,25 @@ import java.util.*;
 
 public class PlayGround
 {  
-    public static int N = 7;
+    public static class Edge implements Comparable<Edge>
+    {
+        int src;
+        int dst;
+        int wt;
+        Edge(int src,int dst,int wt)
+        {
+            this.src = src;
+            this.dst = dst;
+            this.wt  = wt;
+        }
+
+        @Override
+        public int compareTo(Edge obj)
+        {
+            return this.wt - obj.wt;
+        }
+    }
+    public static int N = 4;
     public static int[] parent = new int[N];
     public static int[] rank = new int[N];
 
@@ -43,17 +61,42 @@ public class PlayGround
         }
     }
 
+    public static void krushkalsAlgo(int[][] edges,int V)
+    {
+        int minCost = 0;
+        make();
+        ArrayList<Edge> list = new ArrayList<>();
+
+        for(int i=0; i<edges.length; i++)
+        {
+            int src = edges[i][0];
+            int dst = edges[i][1];
+            int wt = edges[i][2];
+            list.add(new Edge(src,dst,wt));
+        }
+        Collections.sort(list);
+
+        for(int i=0; i<V-1; i++)
+        {
+            Edge e = list.get(i);
+            int a = e.src;
+            int b = e.dst;
+            a = find(a);
+            b = find(b);
+            if(a!=b)
+            {
+                union(a,b);
+                minCost+=e.wt;
+            }
+        }
+
+        System.out.println("Minimum Cost using Krushkals algo = "+minCost);
+    }
     public static void main(String args[])
     {
-        make();
-        System.out.println(find(3));
-        union(1,3);
-        System.out.println(find(3));
-        union(2,4);
-        union(3,6);
-        union(1,4);
-        System.out.println(find(3));
-        System.out.println(find(4));
-        union(1,5);
+        int V = 4;
+        int edges[][] = {{0,1,10},{0,2,15},{0,3,30},{1,3,40},{2,3,50}};
+        
+        krushkalsAlgo(edges,V);
     }
 }
