93ad0cd50f3229bd656102404bd12f6b6398baeb
Rahul Warkade <rahulwarkade954@gmail.com>
2024-05-03 02:59:30 +0530

    Pascal Triangle with O(n^2)

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -2,42 +2,30 @@ import java.util.*;
 
 public class Java_PlayGround
 {
-	public static int factorial(int n)
+
+	public static void pascalTriangle(int n)
 	{
-		int  fact = 1;
 
-		for(int i=1; i<=n; i++)
-			fact*=i;
-		return fact;
-	}
-
-	public static int nCr(int row, int col)
-	{
-		int ncr =1;
-
-		for(int i=1; i<=col; i++)
-		{
-			ncr *= row;
-			row--;
-			ncr /= i;
+		for(int j=1; j<=n; j++)
+		{	
+			int ans = 1;	
+		    System.out.print(ans+" ");
+			for(int i=1; i<j; i++)
+			{
+				ans*= (j-i);
+				ans/=i;
+				System.out.print(ans+" ");
+			}
+			System.out.println();
 		}
-
-		return ncr;
-
-	}
-
-	public static int pascal(int r,int c)
-	{
-		return nCr(r-1,c-1);
 	}
 	public static void main(String [] args)
 	{
-		int r,c;
+		int n;
 		Scanner jin = new Scanner(System.in);
 
-		r = jin.nextInt();
-		c = jin.nextInt();
-
-		System.out.println("The Pascal value is = "+pascal(r,c));
+		n = jin.nextInt();
+		
+		pascalTriangle(n);
 	}
 }
