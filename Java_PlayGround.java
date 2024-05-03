2b77f1d14a11f7d314331c4f467eb3893de22114
Rahul Warkade <rahulwarkade954@gmail.com>
2024-05-04 03:01:34 +0530

    Factorial of Bigger Number TC = O(N*log(N_factorial)) SC = O(MAX(digits in fact)).

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -2,30 +2,42 @@ import java.util.*;
 
 public class Java_PlayGround
 {
+	public static int[] arr = new int[100];
 
-	public static void pascalTriangle(int n)
+	public static void main(String [] args)
 	{
 
-		for(int j=1; j<=n; j++)
-		{	
-			int ans = 1;	
-		    System.out.print(ans+" ");
-			for(int i=1; i<j; i++)
+		int N;
+		Scanner jin = new Scanner(System.in);
+		N = jin.nextInt();
+
+		arr[0] = 1;
+		int len = 1;
+		int fact = 1;
+		int carry = 0;
+		for(int i=1; i<=N; i++)
+		{
+			int j = 0;
+			while(j<len)
 			{
-				ans*= (j-i);
-				ans/=i;
-				System.out.print(ans+" ");
+				fact = arr[j] * i + carry;
+				arr[j] = fact%10;
+				carry = fact/10;
+				j++;
+			}
+
+			while(carry!=0)
+			{
+				arr[len] = carry%10;
+				carry/=10;
+				len++;
+			}
+
+			for(int k=len-1; k>=0; k--)
+			{
+				System.out.print(arr[k]);
 			}
 			System.out.println();
 		}
-	}
-	public static void main(String [] args)
-	{
-		int n;
-		Scanner jin = new Scanner(System.in);
-
-		n = jin.nextInt();
-		
-		pascalTriangle(n);
 	}
 }
