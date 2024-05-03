d3f07f2a149c91342bb1a7c293fda427e8fa60fc
Rahul Warkade <rahulwarkade954@gmail.com>
2024-05-04 04:19:11 +0530

    Factorial using BigInteger TC = O(N^2) SC = O(1)

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -3,24 +3,21 @@ import java.math.BigInteger;
 import java.math.BigDecimal;
 public class Java_PlayGround
 {
-	public static int[] arr = new int[200];
 
+	public static BigInteger factorial(int n)
+	{
+		BigInteger fact = new BigInteger("1");
+
+		for(int i=2; i<=n; i++)
+			fact = fact.multiply(BigInteger.valueOf(i));
+		return fact;
+	}
 	public static void main(String [] args)
 	{
-
-		BigInteger A = new BigInteger("1999999999222");
-		BigInteger B = new BigInteger("10000000002323");
-
-
-		System.out.println("Addition = "+ A.add(B));
-		System.out.println("Subtraction = "+ A.subtract(B));
-		System.out.println("Multiplication = "+ A.multiply(B));
-		System.out.println("Division = "+ A.divide(B));
-		System.out.println("remainder = "+ A.remainder(B));
-
-		BigDecimal X = new BigDecimal("0.03");
-		BigDecimal Y = new BigDecimal("0.04");
-
-		System.out.println(Y.subtract(X));
+		
+		for(int i=1; i<=24; i++)
+		{
+			System.out.println(factorial(i));
+		}
 	}
 }
