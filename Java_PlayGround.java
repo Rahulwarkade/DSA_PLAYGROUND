1457342f07325d012b972a9d3ad3e4b48ae6ddcb
Rahul Warkade <rahulwarkade954@gmail.com>
2024-05-10 09:05:16 +0530

    Majority Elm with O(n) tc and sc = O(1);

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -1,28 +1,50 @@
 import java.util.*;
 import java.util.Scanner;
-import java.math.BigInteger;
-import java.math.BigDecimal;
+
 public class Java_PlayGround
 {
 
 
-	public static List<Integer> majorityElm(int[] arr, int n)
+	public static void majorityElm(int[] nums)
 	{
-		HashMap<Integer,Integer> hm = new HashMap<>();
-		List<Integer> ans = new ArrayList<>();
+		int n = nums.length;
+		int cnt1,cnt2,elm1,elm2;
+		cnt1 = cnt2=0;
+		elm1 =elm2 = Integer.MAX_VALUE;
 
 		for(int i=0; i<n; i++)
 		{
-			hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+			if(cnt1==0 && nums[i]!=elm2)
+			{
+				cnt1 = 1;
+				elm1 = nums[i];
+			}
+			else if(cnt2==0 && nums[i]!=elm1)
+			{
+				cnt2 = 1;
+				elm2 = nums[i];
+			}
+			else if(nums[i]==elm1) cnt1++;
+			else if(nums[i]==elm2) cnt2++;
+			else{
+				cnt1--;
+				cnt2--;
+			}
 		}
 
-		for(Integer key : hm.keySet())
+		List<Integer> ls = new ArrayList<>();
+		cnt1 = cnt2 = 0;
+		for(int i=0; i<n; i++)
 		{
-			if(hm.get(key)>n/3)
-				ans.add(key);
+			if(nums[i]==elm1) cnt1++;
+			if(nums[i]==elm2) cnt2++;
 		}
+		int mini = (n/3)+1;
 
-		return ans;
+		if(cnt1>=mini) ls.add(elm1);
+		if(cnt2>=mini) ls.add(elm2);
+
+		System.out.println(ls);
 	}
 	public static void main(String [] args)
 	{
@@ -37,13 +59,7 @@ public class Java_PlayGround
 			arr[i] = sc.nextInt();
 
 		
-
-		List<Integer> list = new ArrayList<>();
-
-		list = majorityElm(arr,n);
-
-		for(Integer value : list)
-			System.out.println(value);
+		majorityElm(arr);
 
 	}
 }
