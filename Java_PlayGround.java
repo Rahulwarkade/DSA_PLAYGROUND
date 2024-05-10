172039d3e84c21e841748e144a48c7bec67ad06c
Rahul Warkade <rahulwarkade954@gmail.com>
2024-05-10 08:45:48 +0530

    Majority element using HashMap tc = O(n) sc = O(n)

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -1,23 +1,49 @@
 import java.util.*;
+import java.util.Scanner;
 import java.math.BigInteger;
 import java.math.BigDecimal;
 public class Java_PlayGround
 {
 
-	public static BigInteger factorial(int n)
-	{
-		BigInteger fact = new BigInteger("1");
 
-		for(int i=2; i<=n; i++)
-			fact = fact.multiply(BigInteger.valueOf(i));
-		return fact;
+	public static List<Integer> majorityElm(int[] arr, int n)
+	{
+		HashMap<Integer,Integer> hm = new HashMap<>();
+		List<Integer> ans = new ArrayList<>();
+
+		for(int i=0; i<n; i++)
+		{
+			hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+		}
+
+		for(Integer key : hm.keySet())
+		{
+			if(hm.get(key)>n/3)
+				ans.add(key);
+		}
+
+		return ans;
 	}
 	public static void main(String [] args)
 	{
+		int n;
+
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for(int i=0; i<n; i++)
+			arr[i] = sc.nextInt();
+
 		
-		for(int i=1; i<=24; i++)
-		{
-			System.out.println(factorial(i));
-		}
+
+		List<Integer> list = new ArrayList<>();
+
+		list = majorityElm(arr,n);
+
+		for(Integer value : list)
+			System.out.println(value);
+
 	}
 }
