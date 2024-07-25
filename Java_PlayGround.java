aa7af3bd1a0bbdc51764dfc0903b39a3d91065ee
Rahul Warkade <rahulwarkade954@gmail.com>
2024-07-25 08:38:05 +0530

    zero one knapsack memoization

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Java_PlayGround
 {
  
-    public static int _01Knapsack(int[] val,int[] wt,int W,int item)
+    public static int _01Knapsack(int[] val,int[] wt,int W,int item,int[][] dp)
     {
         if(item==val.length || W<=0) return 0;
 
         if(dp[item][W]!=-1) return dp[item][W];
 
         if(W>=wt[item])
-            return dp[item][W] = Math.max(val[item] + _01Knapsack(val,wt,W-wt[item],item+1),_01Knapsack(val,wt,W,item+1));
+            return dp[item][W] = Math.max(val[item] + _01Knapsack(val,wt,W-wt[item],item+1,dp),_01Knapsack(val,wt,W,item+1,dp));
         else
-            return dp[item][W] = _01Knapsack(val,wt,W,item+1);
+            return dp[item][W] = _01Knapsack(val,wt,W,item+1,dp);
     }
 
     public static void main(String args[])
@@ -25,7 +25,7 @@ class Java_PlayGround
         int[] val = new int[N];
         int[] wt = new int[N];
 
-        int[][] dp = new int[N][N];
+        int[][] dp = new int[N][7+1];
         for(int i=0; i<N; i++)
             Arrays.fill(dp[i],-1);
 
@@ -41,7 +41,7 @@ class Java_PlayGround
             wt[i] = sc.nextInt();
         }
 
-        System.out.println(_01Knapsack(val,wt,7,0));
+        System.out.println(_01Knapsack(val,wt,7,0,dp));
     }
 }
 
