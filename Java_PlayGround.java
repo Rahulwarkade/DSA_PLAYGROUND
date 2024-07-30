31fe1a2daa5d3f66dc177da7641c308bd4c5e14d
Rahul Warkade <rahulwarkade954@gmail.com>
2024-07-31 04:49:54 +0530

    unbounded Knapsack

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -1,78 +1,27 @@
 import java.util.*;
 
-class Java_PlayGround
-{
- 
-    public static void printTable(int [][] tab)
+public class Java_PlayGround {
+
+
+    public static int unboundedKnapsack(int[] val,int[] wt,int W,int item)
     {
-        for(int i=0; i<tab.length; i++)
+
+        if(W<=0 || item==wt.length) return 0;
+
+        if(wt[item]<=W)
         {
-            for (int j=0; j<tab[0].length; j++) {
-                System.out.print(tab[i][j]+" ");
-            }
-            System.out.println();
+            return Math.max(val[item]+unboundedKnapsack(val,wt,W-wt[item],item),unboundedKnapsack(val,wt,W,item+1));
+        }
+        else{
+            return unboundedKnapsack(val,wt,W,item+1);
         }
     }
-    public static int tabulation(int[] val,int[] wt,int W)
-    {
-        int N = val.length;
-        int table[][] = new int[N+1][W+1];
 
-        for(int i=0; i<=N; i++)
-        {
-            table[i][0] = 0;
-        }
-        for(int i=0; i<=W; i++)
-        {
-            table[0][i] = 0;
-        }
+    public static void main(String[] args) {
+    
+        int[] val = {15,14,10,45,30};
+        int[] wt = {2,5,1,3,4};
 
-        for(int i=1; i<=N; i++)
-        {
-            for(int j=1; j<=W; j++)
-            {
-                int cost = val[i-1];
-                int weight = wt[i-1];
-                if(weight<=j)
-                {
-                    int ans1 = cost + table[i-1][j-weight];
-                    int ans2 = table[i-1][j];
-                    table[i][j] = Math.max(ans1,ans2);
-                }
-                else
-                {
-                    table[i][j] = table[i-1][j];
-                }
-            }
-        }
-        printTable(table);
-
-
-        return table[N][W];
-    }
-    public static void main(String args[])
-    {
-        int N;
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-
-        int[] val = new int[N];
-        int[] wt = new int[N];
-
-
-
-        for(int i=0; i<N; i++)
-        {
-            val[i] = sc.nextInt();
-        }
-
-        for(int i=0; i<N; i++)
-        {
-            wt[i] = sc.nextInt();
-        }
-
-        System.out.println(tabulation(val,wt,15));
+        System.out.println(unboundedKnapsack(val,wt,7,0));
     }
 }
-
