23319b5960bc86ba51a611d6721e576459eca675
Rahul Warkade <rahulwarkade954@gmail.com>
2024-08-01 15:51:01 +0530

    Wild Card Matching

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -2,27 +2,33 @@ import java.util.*;
 
 public class Java_PlayGround{
 
-    public static int editDistance(String word1, String word2)
+    public static boolean wildCardMatching(String pattern1, String pattern2)
     {
-        int N = word1.length();
-        int M = word2.length();
+        int N = pattern1.length();
+        int M = pattern1.length();
+        boolean[][] dp = new boolean[N+1][M+1];
+        dp[0][0] = true;
+        for(int i=1; i<=N; i++) dp[0][i] = false;
 
-        int[][] dp = new int[N+1][M+1];
-
-        for(int i=0; i<=N; i++) dp[0][i] = i;
-        for(int i=1; i<=M; i++) dp[i][0] = i;
+        for(int i=1; i<=M; i++)
+        {
+            if(pattern2.charAt(i)=='*')
+                dp[i][0] = dp[i-1][0];
+            else 
+                dp[i][0] = false;
+        }
 
         for(int i=1; i<=N; i++)
         {
             for(int j=1; j<=M; j++)
             {
-                if(word1.charAt(i-1)==word2.charAt(j-1))
+                if(pattern1.charAt(i-1)==pattern2.charAt(j-1) || pattern1.charAt(i-1)=='?')
                 {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else 
                 {
-                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) + 1;
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
                 }
             }
         }
@@ -31,10 +37,10 @@ public class Java_PlayGround{
 
     public static void main(String[] args) {
     
-        String str1 = "insertion";
-        String str2 = "execution";
+        String str1 = "baaabaad";
+        String str2 = "*****ba*ad";
 
 
-        System.out.println(editDistance(str1,str2));
+        System.out.println(wildCardMatching(str1,str2));
     }
 }
