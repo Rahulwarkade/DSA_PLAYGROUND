80c888992b80213264377aacdcb14e323045ed72
Rahul Warkade <rahulwarkade954@gmail.com>
2024-08-01 15:06:45 +0530

    Longest common SubString tabulation

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class Java_PlayGround {
+public class Java_PlayGround{
+
 
     public static int lcsTab(String str1,String str2)
     {
-        int N,M;
-        N = str1.length();
-        M = str2.length();
+        int N = str1.length();
+        int M = str2.length();
+        int ans = Integer.MIN_VALUE;
         int[][] dp = new int[N+1][M+1];
-
         for(int i=0; i<=N; i++)
         {
             for(int j=0; j<=M; j++)
@@ -23,21 +23,21 @@ public class Java_PlayGround {
             {
                 if(str1.charAt(i-1)==str2.charAt(j-1))
                 {
-                    dp[i][j] = dp[i-1][j-1] +1;
+                    dp[i][j] = dp[i-1][j-1]+1;
                 }
-                else
+                else 
                 {
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                    dp[i][j] = 0;
                 }
+                ans = Math.max(ans,dp[i][j]);
             }
         }
-        return dp[N][M];
+        return ans;
     }
-
     public static void main(String[] args) {
     
         String str1 = "abcde";
-        String str2 = "ace";
+        String str2 = "acde";
 
 
         System.out.println(lcsTab(str1,str2));
