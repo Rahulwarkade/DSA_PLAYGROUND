373168c4cce52fefc129b7f98575d9a0f2938add
Rahul Warkade <rahulwarkade954@gmail.com>
2024-08-02 19:49:27 +0530

    Catalan Number using Tabulation

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -2,26 +2,23 @@ import java.util.*;
 
 public class Java_PlayGround{
 
-    public static int catalanNumber(int n,int[] dp)
+    public static int catalanNumberTab(int n)
     {
-        if(dp[n]!=-1) return dp[n];
-        if(n==1 || n==0) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
 
-        int catalan = 0;
-
-        for(int i=0; i<n; i++)
+        for(int i=2; i<=n; i++)
         {
-            catalan += catalanNumber(i,dp) * catalanNumber(n-i-1,dp);
-        }
-
-         return dp[n] = catalan;
+            for(int j=0; j<i; j++)
+            {
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        } 
+        return dp[n];
     }
     public static void main(String[] args) {
-        int n =4;
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        System.out.println(catalanNumber(n,dp));
+        System.out.println(catalanNumberTab(4));
 
     }
 }
