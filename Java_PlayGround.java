9f58b976844a445672c5ce0a420e5499ebdf6473
Rahul Warkade <rahulwarkade954@gmail.com>
2024-09-01 16:24:58 +0530

    Dijkstra's Algorithm|| Single src shortest Path

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -2,7 +2,25 @@ import java.util.*;
 
 public class Java_PlayGround{
 
-    public static void graphMap(HashMap<Integer,ArrayList<Integer>> graph)
+    public static class Edge implements Comparable<Edge>
+    {
+        int node;
+        int weight;
+
+        public Edge(int node,int weight)
+        {
+            this.node = node;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge vertex)
+        {
+            return this.weight - vertex.weight;
+        }
+
+    }
+    public static void graphMap(HashMap<Integer,ArrayList<Edge>> graph)
     {
         int V,E;
         Scanner sc = new Scanner(System.in);
@@ -12,9 +30,10 @@ public class Java_PlayGround{
 
         for(int i=0; i<E; i++)
         {
-            int x,y;
+            int x,y,z;
             x = sc.nextInt();
             y = sc.nextInt();
+            z = sc.nextInt();
             if(!graph.containsKey(x)){
                 graph.put(x,new ArrayList<>());
             }
@@ -23,45 +42,100 @@ public class Java_PlayGround{
             }
 
             if(x!=y){
-            graph.get(x).add(y);
+                graph.get(x).add(new Edge(y,z));
             // graph.get(y).add(x);     
             }
         }
-    }
+    }   
 
-    public static void getAllPathUtil(HashMap<Integer,ArrayList<Integer>> graph,int src,int dest, ArrayList<Integer> path)
+    public static void bfsUtil(HashMap<Integer,ArrayList<Edge>> graph,HashMap<Integer,Boolean> visited,int root)
     {
-        path.add(src);
-        if(src == dest)
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        visited.put(root,true);
+        while(!q.isEmpty())
         {
-            for(Integer value : path)
-                System.out.print(value+" ");
-            System.out.println();
-            return;
+            int node = q.remove();
+            System.out.print(node + " ");
+            for(Edge child : graph.get(node))
+            {
+                if(!visited.get(child.node))
+                {
+                    q.add(child.node);
+                    visited.put(child.node,true);
+                }
+            }
         }
 
-        for(Integer child : graph.get(src))
-        {
-            getAllPathUtil(graph,child,dest,path);
-            path.remove(path.size()-1);
-        }
     }
-
-    public static void getAllPath(HashMap<Integer,ArrayList<Integer>> graph)
+    public static void bfs(HashMap<Integer,ArrayList<Edge>> graph)
     {
-        int src = 50;
-        int dest = 24;
-        ArrayList<Integer> path = new ArrayList<>();
+        HashMap<Integer,Boolean> visited = new HashMap<>();
 
-        getAllPathUtil(graph,src,dest,path);
+        for(Integer key : graph.keySet())
+        {
+            visited.put(key,false);
+        }
+        
+        for(Integer key : visited.keySet())     
+        {
+            if(!visited.get(key))
+            {
+                bfsUtil(graph,visited,key);
+            }
+        }   
+
     }
+
+    public static void dijkstrasAlgo(HashMap<Integer,ArrayList<Edge>> graph,int source)
+    {
+        HashMap<Integer,Integer> distance = new HashMap<>();
+        HashMap<Integer,Boolean> visited = new HashMap<>();
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for(Integer key : graph.keySet())
+        {
+            distance.put(key,Integer.MAX_VALUE);
+            visited.put(key,false);
+        }
+        distance.put(source,0);
+        pq.add(new Edge(source,0));
+
+        while(!pq.isEmpty())
+        {
+            Edge vertex = pq.remove();
+
+            if(!visited.get(vertex.node))
+            {      
+                System.out.println(vertex.node +" = "+distance.get(vertex.node));
+                visited.put(vertex.node,true);
+
+                for(Edge child : graph.get(vertex.node))
+                {
+                    int u = vertex.node;
+                    int v = child.node;
+                    int wt = child.weight;
+                    int newDistance = distance.get(u)+wt;
+                    if( newDistance < distance.get(v))
+                    {
+                        distance.put(v,newDistance);
+                        pq.add(new Edge(v,newDistance));
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+
     public static void main(String[] args) {
-        HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
+        HashMap<Integer,ArrayList<Edge>> graph = new HashMap<>();
 
         graphMap(graph);
 
-        getAllPath(graph);
-
+        dijkstrasAlgo(graph,0);
     }
 
 }
