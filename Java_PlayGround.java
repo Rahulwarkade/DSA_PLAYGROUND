b72a2a667c2dea5d12a0e32f3e3e4beb75822c8d
Rahul Warkade <rahulwarkade954@gmail.com>
2024-09-05 12:17:40 +0530

    Minimum cost Connecting city

--- Java_PlayGround.java
+++ Java_PlayGround.java
@@ -1,152 +1,67 @@
 import java.util.*;
 
-public class Java_PlayGround{
+public class Java_PlayGround
+{
 
     public static class Edge implements Comparable<Edge>
     {
-        int src;
+        int cost;
         int dest;
-        int weight;
-
-        public Edge(int src,int dest,int weight)
+        public Edge(int cost,int dest)
         {
-            this.src = src;
+            this.cost = cost;
             this.dest = dest;
-            this.weight = weight;
         }
 
         @Override
-        public int compareTo(Edge vertex)
+        public int compareTo(Edge obj2)
         {
-            return this.weight - vertex.weight;
+            return this.cost - obj2.cost;
         }
-
     }
-    public static void graphMap(HashMap<Integer,ArrayList<Edge>> graph)
+
+    public static int miniCostConnectingCity(int[][] cities)
     {
-        int V,E;
-        Scanner sc = new Scanner(System.in);
-        V = sc.nextInt();
-        E = sc.nextInt();
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[cities.length];
+        int result = 0;
+        Arrays.fill(visited,false);
 
+        pq.add(new Edge(0,0));
 
-        for(int i=0; i<E; i++)
+        while(!pq.isEmpty())
         {
-            int x,y,z;
-            x = sc.nextInt();
-            y = sc.nextInt();
-            z = sc.nextInt();
-            if(!graph.containsKey(x)){
-                graph.put(x,new ArrayList<>());
-            }
-            if(!graph.containsKey(y)){
-                graph.put(y,new ArrayList<>());
-            }
+            Edge curr = pq.remove();
 
-            if(x!=y){
-                graph.get(x).add(new Edge(x,y,z));
-                // graph.get(y).add(new Edge(y,x,z));     
-            }
-        }
-    }   
-
-    public static void bfsUtil(HashMap<Integer,ArrayList<Edge>> graph,HashMap<Integer,Boolean> visited,int root)
-    {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(root);
-        visited.put(root,true);
-        while(!q.isEmpty())
-        {
-            int node = q.remove();
-            System.out.print(node + " ");
-            for(Edge child : graph.get(node))
+            if(!visited[curr.dest])
             {
-                if(!visited.get(child.dest))
+                visited[curr.dest] = true;
+
+                result += curr.cost;
+
+                for(int i=0; i<cities[0].length; i++)
                 {
-                    q.add(child.dest);
-                    visited.put(child.dest,true);
+                    if(cities[i][curr.dest]!=0)
+                    {
+                        pq.add(new Edge(cities[i][curr.dest],i));
+                    }
                 }
             }
         }
 
-    }
-    public static void bfs(HashMap<Integer,ArrayList<Edge>> graph)
-    {
-        HashMap<Integer,Boolean> visited = new HashMap<>();
-
-        for(Integer key : graph.keySet())
-        {
-            visited.put(key,false);
-        }
-        
-        for(Integer key : visited.keySet())     
-        {
-            if(!visited.get(key))
-            {
-                bfsUtil(graph,visited,key);
-            }
-        }   
-
-    }
-
-    public static class Info{
-        int v;
-        int cost;
-        int stops;
-
-        public Info(int v,int cost,int stops)
-        {
-            this.v = v;
-            this.cost = cost;
-            this.stops = stops;
-        }
-    }
-    public static int cheapestFlight(HashMap<Integer,ArrayList<Edge>> graph,int src,int dest,int K)
-    {
-        Queue<Info> q = new LinkedList<>();
-        HashMap<Integer,Integer> distance = new HashMap<>();
-
-        for(Integer key : graph.keySet())
-        {
-            distance.put(key,Integer.MAX_VALUE);
-        }
-
-        distance.put(src,0);
-        q.add(new Info(src,0,0));
-
-        while(!q.isEmpty())
-        {
-            Info curr = q.remove();
-
-            if(curr.stops>K) break;
-
-            for(Edge child : graph.get(curr.v))
-            {
-                int u = child.src;
-                int v = child.dest;
-                int cost = child.weight;
-                int newCost = curr.cost+cost;
-                if(newCost<distance.get(v) && curr.stops<=K)
-                {
-                    distance.put(v,newCost);
-                    q.add(new Info(v,newCost,curr.stops+1));
-                }
-            }
-        }
-
-        if(distance.get(dest)==Integer.MAX_VALUE) return -1;
-
-        return distance.get(dest);
+        return result;
     }
     public static void main(String[] args) {
-        HashMap<Integer,ArrayList<Edge>> graph = new HashMap<>();
 
-        graphMap(graph);
+        int[][] cities = {
+            {0,1,2,3,4},
+            {1,0,5,0,7},
+            {2,5,0,6,0},
+            {3,0,6,0,0},
+            {4,7,0,0,0}
+        };
 
-        System.out.println(cheapestFlight(graph,0,3,1));
-
-        bfs(graph);
-       
+        System.out.println(miniCostConnectingCity(cities));
     }
 
 }
